788ae3e3e3b649bbc1e6d3eec365fe8982ec203d
Yaroslav Kurilin <yaroslav.kurilin.z@gmail.com>
2022-02-11 14:52:02 +0200

    Update react router to > 5.1.0

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
-const About = () => (
-  <div>
-    <h2>About Page</h2>
-  </div>
-);
+const About = () => {
+  const history = useHistory();
+
+  const redirectMain = () => {
+    history.push('/');
+  }
+   return (
+    <div>
+      <h2>About Page</h2>
+      <button onClick={redirectMain}>Go to Home</button>
+    </div>
+   )
+
+}//
 
 export default About;
