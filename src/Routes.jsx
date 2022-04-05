26c6c3519824f0ddcabffd8bbbc5242edbabc889
Yaroslav Kurilin <yaroslav.kurilin.z@gmail.com>
2022-04-05 11:47:05 +0300

    Add router guard component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, MemosIndex, MemosSingleFull, NotFound, Login, SignUp } from './containers';
+import RouterGuard from './containers/_Common/_RouterGuard';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/login" component={Login} />
     <Route path="/signup" component={SignUp} />
-    <Route path="/memos" component={MemosIndex} />
-    <Route path="/memo/:ids" component={MemosSingleFull} />
-    <Route path="/memo/add" component={MemosSingleFull} />
+
+    <RouterGuard path="/memos" component={MemosIndex} auth={true} />
+    <RouterGuard path="/memo/:ids" component={MemosSingleFull} auth={true} />
+    <RouterGuard path="//memo/add" component={MemosSingleFull} auth={true} />
     <Route component={NotFound} />
   </Switch>
 );
